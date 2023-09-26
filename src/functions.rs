1f1ddf0c4322f1655196ba7fb6e1ee3f09d24752
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-09-26 19:27:24 +0600

    Integration Tests

--- src/functions.rs
+++ src/functions.rs
@@ -1,1 +1,7 @@
-
+pub fn validate(v: i32) -> Result<(), ()> {
+    if v < 0 {
+        Err(())
+    } else {
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,11 @@
 pub mod functions;
 
-/// Returns the length of the given string.
-/// ```
-/// use unit_testing::get_string_len;
-/// assert_eq!(get_string_len("Hello World".to_string()), 11);
-/// assert_eq!(get_string_len("".to_string()), 10); // ! main' panicked at 'assertion failed: `(left == right)` left: `0`, right: `10`', src/lib.rs:7:1
-/// ```
+#[test]
+fn test_positive_number() {
+    assert!(functions::validate(100).is_ok());
+}
 
-pub fn get_string_len(s: String) -> usize {
-    if s.is_empty() {
-        return 0;
-    }
-    s.len()
+#[test]
+fn test_negative_number() {
+    assert!(functions::validate(-100).is_ok());
 }
