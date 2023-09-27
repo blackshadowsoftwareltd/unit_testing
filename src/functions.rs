cf95827fb9b980f427eedf5a46689a518ded3603
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-09-27 07:35:00 +0600

    googletest crates example

--- src/functions.rs
+++ src/functions.rs
@@ -1,7 +1,7 @@
-pub fn validate(v: i32) -> Result<(), ()> {
-    if v < 0 {
-        Err(())
-    } else {
-        Ok(())
-    }
+pub fn sum_of_a(a: i32) -> i32 {
+    a + a
+}
+
+pub fn arr_of_b(b: i32) -> Vec<i32> {
+    (0..b).collect()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,28 @@
 pub mod functions;
 
 #[test]
-fn test_positive_number() {
-    assert!(functions::validate(100).is_ok());
+fn fails_and_panics() {
+    googletest::assert_that!(functions::sum_of_a(5), googletest::prelude::eq(10));
+}
+
+#[googletest::test]
+fn two_logged_failures() {
+    googletest::expect_that!(functions::sum_of_a(5), googletest::prelude::eq(10));
 }
 
 #[test]
-fn test_negative_number() {
-    assert!(functions::validate(-100).is_ok());
+fn fails_immediately_without_panic() -> googletest::Result<()> {
+    googletest::verify_that!(functions::sum_of_a(5), googletest::prelude::eq(10))?;
+    Ok(())
 }
+
+/*
+#[test]
+fn simple_assertion() -> googletest::Result<()> {
+    googletest::expect_that!(
+        functions::arr_of_b(10),
+        googletest::prelude::contains(googletest::prelude::ge(0))
+    );
+    Ok(())
+}
+*/
