23e68acdd3e34ea7590c99452107a0d487d40f41
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-09-29 00:33:35 +0600

    Parametrize test using rstest crates

---
+++ src/functions.rs
@@ -0,0 +1,3 @@
+pub fn sum_of_arr(l: u32) -> u32 {
+    (0..l).sum()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,12 @@
-// pub mod functions;
+pub mod functions;
 use rstest::*;
 
 #[rstest]
-fn is_equal_10(number_10: i32) {
-    assert_eq!(number_10, 15);
-}
-
-#[rstest]
-fn is_not_equal_10(number_10: i32) {
-    assert_ne!(number_10, 15);
-}
-
-#[fixture]
-pub fn number_10() -> i32 {
-    10
+#[case(1, 10)]
+#[case(2, 10)]
+#[case(3, 10)]
+#[case(4, 10)]
+#[case(5, 10)]
+fn fibonacci(#[case] input: u32, #[case] expected: u32) {
+    assert_eq!(functions::sum_of_arr(input), expected);
 }
