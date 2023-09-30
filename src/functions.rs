e299b1e3543e2d58f26c9fdd086a008845d3a526
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2023-09-30 11:21:31 +0600

    use template for reuse test case using rstest_reuse crates

--- src/functions.rs
+++ src/functions.rs
@@ -1,3 +1,3 @@
-pub fn sum_of_arr(l: u32) -> u32 {
+pub fn sum_of_arr(l: i32) -> i32 {
     (0..l).sum()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,23 @@
 pub mod functions;
 use rstest::*;
+use rstest_reuse::{self, *};
 
+// ? custom template for reuse test case
+#[template]
 #[rstest]
-#[case(1, 10)]
-#[case(2, 10)]
 #[case(3, 10)]
-#[case(4, 10)]
 #[case(5, 10)]
-fn fibonacci(#[case] input: u32, #[case] expected: u32) {
-    assert_eq!(functions::sum_of_arr(input), expected);
+#[case(7, 10)]
+fn test_template(#[case] a: i32, #[case] b: i32) {}
+
+// ? applying a custom template on a test function
+#[apply(test_template)]
+fn is_valid(a: i32, b: i32) {
+    assert_eq!(functions::sum_of_arr(a), b);
+}
+
+// ? applying a custom template on another test function
+#[apply(test_template)]
+fn is_equal(a: i32, b: i32) {
+    assert_eq!(functions::sum_of_arr(a), b)
 }
