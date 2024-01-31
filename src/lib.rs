b828c07c723ae8f373ab78200307e2c1f727d8de
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2024-01-31 19:15:49 +0600

    hash with crc32fast

--- src/lib.rs
+++ src/lib.rs
@@ -22,14 +22,21 @@ mod _hash_test {
 
     #[tokio::test]
     async fn do_hash() {
-        let hello_world = hash_code(&"Hello, world!");
-        println!("Hello, world! hash: {:x}", hello_world);
-        assert_eq!(hello_world, hash_code(&"Hello, world!"));
+        let std_hash = hash_code(&"Hello, world!");
+        let crc_32 = crc32_hash(&"Hello, world!");
+        println!("{:?} == {:?}", std_hash, crc_32);
+        assert_eq!(std_hash, crc_32 as u64);
     }
 
     fn hash_code<T: Hash>(t: &T) -> u64 {
         let mut s = DefaultHasher::new();
         t.hash(&mut s);
         s.finish()
+    }
+
+    fn crc32_hash<T: std::hash::Hash>(data: &T) -> u32 {
+        let mut hasher = crc32fast::Hasher::new();
+        data.hash(&mut hasher); // Utilize the Hash trait for generic hashing
+        hasher.finalize()
     }
 }
